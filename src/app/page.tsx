6b45914d8654c774f2d4a0ab37afc8ef1a3378d9
allenpeng05 <157744889+allenpeng05@users.noreply.github.com>
2025-02-20 00:29:01 -0800

    Create home page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/login">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition">
-            Go to Login
-          </button>
-        </Link>
-      </main>
+    <div className="flex h-screen">
+      <div className="w-1/2 bg-gray-100 p-4">
+        <h1> Your Planned Classes</h1>
+      </div>
+      <div className="w-1/2 bg-gray-200 p-4">
+        <h1>Friends' Planned Classes</h1>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,32 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <nav className="flex items-center justify-between px-6 py-4 bg-blue-600 text-white shadow-md">
-        <div className="flex container mx-auto flex justify-between items-center">
-          <Link href="/" className="text-xl font-bold flex">
-          <Image src="/favicon.png" alt="Bruin Class Mates Logo" width={40} height={40} />
-          <p className="px-3">BruinClassMates</p></Link>
-          <div className="flex gap-4">
-            <Link href="/" className="hover:text-gray-300">Home</Link>
-            <Link href="/login" className="hover:text-gray-300">Login</Link>
-            <Link href="/register" className="hover:text-gray-300">Register</Link>
-          </div>
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-blue-600 text-white shadow-md">
+      <div className="flex container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold flex">
+          <Image
+            src="/favicon.png"
+            alt="Bruin Class Mates Logo"
+            width={40}
+            height={40}
+          />
+          <p className="px-3">BruinClassPals</p>
+        </Link>
+        <div className="flex gap-4">
+          <Link href="/" className="hover:text-gray-300">
+            Home
+          </Link>
+          <Link href="/login" className="hover:text-gray-300">
+            Login
+          </Link>
+          <Link href="/register" className="hover:text-gray-300">
+            Register
+          </Link>
         </div>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
