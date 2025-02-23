7f8ea82c73aa192290e662b120d5b7a1c7107168
allenpeng05 <157744889+allenpeng05@users.noreply.github.com>
2025-02-22 23:18:39 -0800

    Finished barebone homepage UI design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,129 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import ClassCard from "@/components/ClassCard";
+import { Search } from "lucide-react";
 
 export default function Home() {
+  const [classes, setClasses] = useState([
+    {
+      classId: "CS143",
+      className: "Database Systems",
+      professor: "Ryan Rosario",
+    },
+    {
+      classId: "CS35L",
+      className: "Software Construction",
+      professor: "Paul Eggert",
+    },
+    {
+      classId: "Math 115A",
+      className: "Linear Algebra",
+      professor: "Wern Yeong",
+    },
+  ]);
+
+  const [friendsClasses, setFriendsClasses] = useState([
+    {
+      friendName: "Alex",
+      classes: [
+        {
+          classId: "CS118",
+          className: "Computer Network Fundamentals",
+          professor: "Lixia Zhang",
+        },
+        {
+          classId: "CS35L",
+          className: "Software Construction",
+          professor: "Paul Eggert",
+        },
+        {
+          classId: "MATH 151A",
+          className: "Applied Numerical Methods",
+          professor: "Deanna Needell",
+        },
+      ],
+    },
+    {
+      friendName: "Sam",
+      classes: [
+        {
+          classId: "CS180",
+          className: "Introduction to Algorithms",
+          professor: "Amit Sahai",
+        },
+        {
+          classId: "CS33",
+          className: "Computer Organization",
+          professor: "Miryung Kim",
+        },
+        {
+          classId: "MATH 170A",
+          className: "Probability Theory",
+          professor: "Wesley Perkins",
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="flex h-screen">
-      <div className="w-1/2 bg-gray-100 p-4">
-        <h1> Your Planned Classes</h1>
-      </div>
+      {/* Planned Classes Container */}
       <div className="w-1/2 bg-gray-200 p-4">
-        <h1>Friends' Planned Classes</h1>
+        <h1 className="text-3xl text-center font-bold">Your Planned Classes</h1>
+        {/* List Container */}
+        <div className="mt-4 p-3 bg-gray-100 shadow-md rounded-lg">
+          <ul className="list-none space-y-2">
+            {classes.map((course, index) => (
+              <ClassCard
+                key={index}
+                classId={course.classId}
+                className={course.className}
+                professor={course.professor}
+              />
+            ))}
+          </ul>
+          {/* Button to Add/Remove a class*/}
+          <button className="mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+            Add / Remove Class
+          </button>
+        </div>
+      </div>
+
+      {/* Friend's Side */}
+      <div className="w-1/2 h-full max-h-screen overflow-y-auto p-4 bg-gray-300">
+        <h1 className="text-3xl text-center font-bold">Friends</h1>
+        {/* Search Bar */}
+        <div className="relative mt-3 mb-4">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full p-2 pr-10 border rounded-md focus:outline-none"
+          />
+          <Search className="absolute right-3 top-2 w-5 text-gray-500" />
+        </div>
+
+        {friendsClasses.map((friend, friendIndex) => (
+          <div
+            key={friendIndex}
+            className="mt-6 p-3 bg-gray-100 shadow-md rounded-lg w-full"
+          >
+            <h2 className="text-xl font-semibold text-center text-gray-700">
+              {friend.friendName}
+            </h2>
+            <ul className="list-none space-y-2">
+              {friend.classes.map((course, index) => (
+                <ClassCard
+                  key={index}
+                  classId={course.classId}
+                  className={course.className}
+                  professor={course.professor}
+                />
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/ClassCard.tsx
@@ -0,0 +1,24 @@
+import { User } from "lucide-react";
+
+export default function ClassCard({
+  classId,
+  className,
+  professor,
+}: {
+  classId: string;
+  className: string;
+  professor: string;
+}) {
+  return (
+    <li className="bg-white shadow-md rounded-lg p-4 w-full">
+      <span className="bg-blue-500 text-white text-sm font-semibold px-2 py-1 rounded">
+        {classId}
+      </span>
+      <h2 className="text-xl font-bold mt-2">{className}</h2>
+      <p className="text-blue-600 font-medium mt-2 flex items-center">
+        <User className="w-5 h-5 mr-2 text-blue-600" />
+        Professor: {professor}
+      </p>
+    </li>
+  );
+}
