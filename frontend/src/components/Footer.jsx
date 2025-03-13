ea53a6c493d144fd7a7437da53fd6e09799a428c
Neil Johnson <neilwj07@gmail.com>
2025-03-12 17:11:10 -0700

    change footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white text-center p-4">
-        <p> 2025 Bruin Class Mates</p>
+        <p> 2025 Bruin Class Pals</p>
       </footer>
     );
   };
