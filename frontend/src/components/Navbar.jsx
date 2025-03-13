073dc6af36cafc50d5c315f6ae5ce4c1f00ac94f
r100agarwal <162980825+r100agarwal@users.noreply.github.com>
2025-03-12 17:21:32 -0700

    added sign out button to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import jwt from "jsonwebtoken";
+import {useRouter} from "next/navigation"
 
 const Navbar = () => {
   const [userId, setUserId] = useState(null);
+  const router = useRouter();
 
   const getUserIdFromToken = () => {
     try {
@@ -33,6 +35,12 @@ const Navbar = () => {
     setUserId(getUserIdFromToken());
   }, []);
 
+  const handleLogout = () => {
+    document.cookie = "token=;path=/;max-age=0;";
+    setUserId(null);
+    router.push("/login");
+  }
+
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-blue-700 text-white shadow-md font-roboto">
       <div className="flex container mx-auto justify-between items-center">
@@ -43,7 +51,7 @@ const Navbar = () => {
             width={40}
             height={40}
           />
-          <p className="px-3 m-2">BruinClassPals</p>
+          <p className="px-2.75 m-2">BruinClassPals</p>
         </Link>
         <div className="flex gap-4">
           {userId ? (
@@ -57,6 +65,12 @@ const Navbar = () => {
               <Link href="/social" className="hover:text-gray-300">
                 Social
               </Link>
+              <button
+                onClick={handleLogout}
+                className="bg-red-500 px-2.5 py-1.5 rounded-lg hover:bg-red-600 transition"
+              >
+                Sign Out
+              </button>
             </>
           ) : (
             <>
